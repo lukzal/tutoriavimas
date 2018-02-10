b868efc1d440f418e6550d512cdfbf7591e29f22
VaidotasRu <36315352+VaidotasRu@users.noreply.github.com>
2018-02-10 22:38:55 +0200

    jquery

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <script src="js/bootstrap.min.js"></script>
-<script src="js/jquery-3.3.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 <nav class="navbar navbar-expand-sm bg-light">
 
