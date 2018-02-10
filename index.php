1cac18e89fc53d933d7fc9cc25aaa8e6986cc816
VaidotasRu <36315352+VaidotasRu@users.noreply.github.com>
2018-02-10 22:37:33 +0200

    jquery/bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-3.3.1.min.js"></script>
+
+<nav class="navbar navbar-expand-sm bg-light">
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 3</a>
+    </li>
+  </ul>
+
+</nav>
+
+
+
+
+
+</body>
+</html>
