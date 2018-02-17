d955d195f3c25326c64a13d792a95984543671fc
VaidotasRu <36315352+VaidotasRu@users.noreply.github.com>
2018-02-17 02:14:27 +0200

    mainpage

---
+++ Testavimas/mainpage/mainpage.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+
+
+
+<!-- searchboxas su submitu, kurį paspaudus bus vykdoma paieška. Vėliau implementuota bus ajax-->
+
+<div id="search">
+<input type="text" name="searchbox" placeholder="Search for people..">
+<input type="submit" name="searchbutton">
+</div>
+
+
+
+
+<div id="maincontainer">
+	<h3>Naujienos pavadinimas</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+
+    <h3>Naujienos pavadinimas</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+</div>
+
+
+
+<!-- sidebar'as su nuorodomis (vėliau bus galima naviguot) -->
+
+<div id="sidebar">
+	<ul>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>
+		<li><a href="https://www.google.lt/">linkas</a>  </li>	
+	</ul>
+
+
+
+
+</div>
+<div></div>
+
+
+<div></div>
+
+
+</body>
+</html>
